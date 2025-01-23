e88cbce731678a113c72a8067602b56af8b46a74
Kledis GJoni <kgjoni@myseneca.ca>
2025-01-23 12:14:10 -0500

    last

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# "Author" Kledis Gjoni
+# "Author ID" kgjoni
+# "Date Created" 2025/1/20
+import sys
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')

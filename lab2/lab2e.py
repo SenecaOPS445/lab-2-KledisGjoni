7378cc10596c3c8611ba937f208389cf7cbc5f39
Kledis GJoni <kgjoni@myseneca.ca>
2025-01-23 11:56:48 -0500

    Add new files fin

---
+++ lab2/lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# "Author" Kledis Gjoni
+# "Author ID" kgjoni
+# "Date Created" 2025/1/20
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+    
+print('blast off!')
